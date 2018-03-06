dc1e06682f9aebbd26ced14849e4bb0400fa6f57
altaymehmet <36675175+altaymehmet@users.noreply.github.com>
2018-03-06 20:51:31 +0300

    Add files via upload

---
+++ uygulama03.py
@@ -0,0 +1,19 @@
+# Hacettepe üniversitesinde bulunan herhangi bir sınıftaki elemanlar
+# tüpler
+demirbaşlar=("projeksiyon","masa","sandalye","tahta")
+# sözlük
+öğrenciler={"mehmet":"21559679","sercan":"21557945","ali":"22956423"}
+
+print(öğrenciler["sercan"])
+print(öğrenciler["mehmet"])
+print(demirbaşlar[2])
+# liste
+değişenler=["laptop","hoparlör","kağıt","yoklama listesi","kalem"]
+
+değişenler[0]="bilgisayar"
+print(öğrenciler["mehmet"])
+
+print(len(değişenler))
+değişenler.append("not defteri")
+öğrenciler["ayşe"]="23134363"
+print(öğrenciler)
